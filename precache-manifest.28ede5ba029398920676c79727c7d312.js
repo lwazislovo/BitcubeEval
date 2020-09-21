49d7003d7145f2e2abd5491ef3defc6afe4778c5
Gugulethu-webdev <mtshaliamo@gmail.com>
2020-09-21 19:50:54 +0200

    Updates

---
+++ precache-manifest.28ede5ba029398920676c79727c7d312.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62037e6ebeb4aa8f9415a33330a61afd",
+    "url": "/BitcubeEval/index.html"
+  },
+  {
+    "revision": "2c5b2a3a859f4388f2f9",
+    "url": "/BitcubeEval/static/css/main.5d628485.chunk.css"
+  },
+  {
+    "revision": "8f26f43a2f0b6edcd536",
+    "url": "/BitcubeEval/static/js/2.0000756c.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/BitcubeEval/static/js/2.0000756c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5b2a3a859f4388f2f9",
+    "url": "/BitcubeEval/static/js/main.dc5e5d04.chunk.js"
+  },
+  {
+    "revision": "94c4dfc0e6a111916d2d",
+    "url": "/BitcubeEval/static/js/runtime-main.2fc4c9ba.js"
+  },
+  {
+    "revision": "cb3739dd485059f490b9af8b9c1e11a8",
+    "url": "/BitcubeEval/static/media/AccountingLogo.cb3739dd.png"
+  },
+  {
+    "revision": "f485ae350d2d874416d633917fc5d6aa",
+    "url": "/BitcubeEval/static/media/AppliedMathsLogo.f485ae35.jpg"
+  },
+  {
+    "revision": "d0279a955192b185c8e4e1fd05953c70",
+    "url": "/BitcubeEval/static/media/ComputerScienceLogo.d0279a95.jpg"
+  },
+  {
+    "revision": "9a9aabfc771b931746409ba0acdc2ad9",
+    "url": "/BitcubeEval/static/media/ElectricalLogo.9a9aabfc.jpeg"
+  },
+  {
+    "revision": "e7bdd8f9117bee27e287975136bfef81",
+    "url": "/BitcubeEval/static/media/MathsLogo.e7bdd8f9.jpeg"
+  },
+  {
+    "revision": "3d03a7ac71a3abd05b53323a4dcacf22",
+    "url": "/BitcubeEval/static/media/MechanicalEngineeringLogo.3d03a7ac.jpg"
+  }
+]);
